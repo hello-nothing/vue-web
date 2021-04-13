a7e40eccc56682863a483eb29be3d8787640f9cc
weirui <15037268535@sina.cn>
2021-04-13 11:08:14 +0800

    脚手架新增登录验证

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,48 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Login from "@/views/login";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "HelloWorld",
+      component: HelloWorld,
+      meta: {
+        title: "首页",
+      }
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        title: "登录",
+        noNeedLogin: true
+      }
     }
   ]
-})
+});
+router.beforeEach((to, from, next) => {
+  if (!to.meta.noNeedLogin) {
+    if (window.localStorage.getItem("isLogin")) {
+      next();
+    } else {
+      next({ path: "/login" });
+    }
+  } else {
+    next();
+  }
+});
+router.afterEach(function(to, from) {
+  document.title = to.meta.title; //跳转后设置页面的title
+});
+
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
+export default router;
